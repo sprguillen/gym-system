9c9a6f4f5ab0829191962284859cc4225e37b099
Monique <monique.dingding@gmail.com>
2018-03-07 19:31:02 +0800

    changed header title

--- application/views/components/header.php
+++ application/views/components/header.php
@@ -13,7 +13,7 @@
     <div class="container d-flex flex-column flex-md-row justify-content-between">
       <a class="py-3 d-none d-md-inline-block font-weight-bold" href="<?php echo base_url(); ?>">
         <img class="header-icon" src="<?php echo base_url('assets/images/fitness.png'); ?>">
-        Gym Master
+        Elevation Fitness
       </a>
       <a class="py-3 d-none pr-4 d-md-inline-block" href="#">Membership</a>
       <a class="py-3 d-none pr-4 d-md-inline-block" href="#">Pricing</a>
