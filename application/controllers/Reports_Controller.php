454c0d98365d7da3b4e67dba7523e23fbbc2cbba
Monique <monique.dingding@gmail.com>
2018-07-10 14:00:10 +0800

    weekly and print functionality

--- application/controllers/Reports_Controller.php
+++ application/controllers/Reports_Controller.php
@@ -52,6 +52,68 @@ class Reports_Controller extends CI_Controller {
     }
 
     /**
+     * Gate sat and sun (first and last day of the week)
+     * @param  $timestamp
+     * @return [Array]      
+     */
+    public function get_sat_sun_dow($timestamp) {
+        $timestamp = (isset($timestamp))? $timestamp: time();
+
+        $day_of_week = date('w', $timestamp);
+        $offset = ($day_of_week < 0)? 6: (int) $day_of_week;
+
+        $days = [];
+
+        $sunday_timestamp = $timestamp - $offset * 86400;
+        $days['sun'] = date('Y-m-d', $sunday_timestamp);
+
+        $saturday_timestamp = $sunday_timestamp + (86400 * 6);
+        $days['sat'] = date('Y-m-d', $saturday_timestamp);
+
+        return $days;
+    }
+
+
+    public function ajax_get_reports_by_week() {
+        $selected_date = $this->input->post('selected_date');
+        $selected_timestamp = strtotime($selected_date);
+
+        $start_day = strtotime($selected_date . '-01');
+        $last_day = strtotime(date('Y-m-t', $selected_timestamp));
+
+
+        $data['last_day'] = $last_day;
+        $data['start_day'] = $start_day;
+        
+        $timestamp = $start_day;
+        $i = $timestamp;
+        $week = [];
+        $count = 0;
+
+        do {
+            $week[$count] = $this->get_sat_sun_dow($timestamp);
+            $timestamp = strtotime('+7 days', $timestamp);
+            
+            $result[$count] = $this->Report_Model->get_membership_payments(NULL, $week[$count]);
+            $count++;
+
+        } while ($timestamp < $last_day);
+
+        $data['week'] = $week;
+        $data['selected_date'] = $selected_date;
+        $data['result'] = $result;
+
+
+        // $data['result'] = $this->Report_Model->get_membership_payments(NULL, $start_day);
+
+
+
+
+
+        echo json_encode($data);
+    }
+
+    /**
      * Method to render template (header - body - footer)
      * @param  [string] $page
      */
--- application/models/Report_Model.php
+++ application/models/Report_Model.php
@@ -11,7 +11,7 @@ class Report_Model extends CI_Model {
         $this->load->database();
     }
 
-    public function get_membership_payments($select_date = NULL) {
+    public function get_membership_payments($select_date = NULL, $between = NULL) {
 
         $sql = "SELECT * FROM membership_payment AS mp
                 JOIN membership AS ms ON ms.id = mp.membership_id
@@ -19,11 +19,16 @@ class Report_Model extends CI_Model {
                 JOIN program_price AS pp ON pp.id = mp.program_price_id
                 JOIN program AS p ON p.id = pp.program_id ";
 
-        if (!isset($select_date)) {
-            $select_date = date('Y-m') . "-%";
+        if (isset($between)) {
+            $sql .= "WHERE mp.payment_date_time BETWEEN '" . $between['sun'] . "' AND '" . $between['sat'] . "'";
+        } else {
+            if (!isset($select_date)) {
+                $select_date = date('Y-m') . "-%";
+            }
+
+            $sql .= "WHERE mp.payment_date_time LIKE '" . $select_date . "' ";
         }
 
-        $sql .= "WHERE mp.payment_date_time LIKE '" . $select_date . "' ";
         $sql .= "ORDER BY mp.payment_date_time DESC";
 
         return $this->db->query($sql)->result();
